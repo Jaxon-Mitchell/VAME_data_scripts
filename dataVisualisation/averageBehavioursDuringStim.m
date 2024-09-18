d8ce5b1cb8603448315627abffd8c887728c8989
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-09-18 15:57:04 +0930

    Updating current WIP to work on from home

--- dataVisualisation/averageBehavioursDuringStim.m
+++ dataVisualisation/averageBehavioursDuringStim.m
@@ -1,10 +1,67 @@
 % This script shows (in 10 ms buckets) the average behaviour a hoverfly
 % displays for any given stimuli
 
+% Get user to select .csv containing VAME motif timeseries
+inputFolder = uigetdir('Select your folder containing motif usage .csv''s');
+
+% Define variables %
 % Stimuli to choose from, select any
 % stimuli = ["Dorsal_Loom_Fast", "Dorsal_Loom_HalfFast", ...
 %           "Dorsal_Loom_Halfslow", "Dorsal_Loom_Slow", ...
 %           "Ventral_Loom_Fast", "Ventral_Loom_HalfFast", ...
 %           "Ventral_Loom_HalfSlow", "Ventral_Loom_Slow", ...
 %           "Dorsal_Loom_control", "Ventral_Loom_control"];
+
+% Define camera frame rate (FPS) and analysis time bucket (ms)
+frameRate = 100;
+timeBucket = 100;
+framesPerBucket = floor((timeBucket * 10^(-3)) / (1 / frameRate));
+
+% Define longest experiment length
+experimentMax = 5; % Seconds
+
+% This string should contain the expected file name format for motif usage
+fileType = "motif_usage_";
+
+% Get user defined community groupings 
+community = returnCommunities();
+
+csvList = dir(fullfile(inputFolder, '*.csv'));
+csvList = {csvList.name};
+
+motifFiles = find(cell2mat(regexp(csvList, fileType)));
+
+for stimulus = 1:length(stimuli)
+    % Pre-calculate the rough size of the analysis file we need
+    behaviourAnalysis = zeros((experimentMax / (timeBucket * 10^(-3))),length(community));
+    % Get only the motif files relevant to our stimuli
+    stimuliFiles = find(cell2mat(regexp(motifFiles, stimuli(stimulus))));
+    % Loop over all experiments and extract community info 
+    for file = 1:length(stimuliFiles)
+        % Load the motif data
+        experiment = readmatrix([inputFolder, '/', csvList{motifFiles(file)}]);
+        % Initialise the frame bucket system
+        bucket = 1;
+        while bucketOffset < size(experiment, 1)
+            % This variable accounts for different bucket indexing
+            bucketOffset = framesPerBucket * (bucket - 1);
+            for frame = 1:framesPerBucket
+                for group = 1:length(community)
+                    if ismember(experiment((bucketOffset + frame), 2), community{group}.motifs)
+                        behaviourAnalysis(bucket, group) = behaviourAnalysis(bucket, group) + 1;
+                        break
+                    end
+                end
+            end
+            bucket = bucket + 1;
+        end
+    end
+    % Normalise our bar data into a percentage variable
+
+    % Plot our average behaviour data here!
+    figure
+    bar(behaviourAnalysis,'stacked')
+end
+
+
 
---
+++ dataVisualisation/returnCommunities.m
@@ -0,0 +1,14 @@
+function community = returnCommunities()
+% Returns the VAME motifs and their community groups as set by the user in
+% this script
+
+% List all your commuities and their motifs here:
+community{1}.motifs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+community{1}.name = 'Flying';
+community{2}.motifs = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+community{2}.name = 'Reacting';
+community{3}.motifs = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+community{3}.name = 'Starfish';
+community{3}.motifs = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+end
+
